dc3e240f6aa2e3b5098b4ee62f93cacfa5f6062d
gdeconcienciacorporal <156134961+gdeconcienciacorporal@users.noreply.github.com>
2024-05-06 07:44:50 -0300

    Commit week 15 & 16

--- script.js
+++ script.js
@@ -1,12 +1,12 @@
 document.addEventListener("DOMContentLoaded", function () {
-actualizarDiasRestantes();
-setInterval(actualizarDiasRestantes, 24*60*60*1000); // Actualizar diario
+  actualizarDiasRestantes();
+  setInterval(actualizarDiasRestantes, 24*60*60*1000); // Actualizar diario
    // Datos para el gráfico de líneas
 const dataLine1 = {
   labels: ['Día 1', 'Día 2', 'Día 3', 'Día 4'], // Etiquetas de los días
   datasets: [{
-    label: 'Total Km recorridos en Semana 13.',
-    data: [15, 15, 10, 25], // Datos de los km recorridos en cada día
+    label: 'Km recorridos en Semana 5',
+    data: [11, 8, 13, 16], // Datos de los km recorridos en cada día
     borderColor: 'rgba(75, 192, 192, 1)',
     fill: true,
     tension: 0.1
@@ -16,8 +16,8 @@ const dataLine1 = {
 const dataLine2 = {
   labels: ['Día 1', 'Día 2', 'Día 3', 'Día 4'], // Etiquetas de los días
   datasets: [{
-    label: 'Total Km recorridos en Semana 14',
-    data: [16, 12, 13, 22], // Datos de los km recorridos en cada día
+    label: 'Km recorridos en Semana 6',
+    data: [10, 14, 14, 18], // Datos de los km recorridos en cada día
     borderColor: 'rgba(153, 102, 255, 1)',
     fill: true,
     tension: 0.1
@@ -50,7 +50,6 @@ const myChartLine2 = new Chart(document.getElementById('myChart-line-2'), {
     }
   }
 });
- 
 
 function actualizarDiasRestantes() {
   // Obtener el título que contiene la fecha objetivo
